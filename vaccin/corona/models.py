6fdac666fae5543ab7091c0fea1b4fee079f4f91
khalillamharchi20 <khalillamharchi20@gmail.com>
2021-02-16 03:13:29 +0100

    scnd commit

--- vaccin/corona/models.py
+++ vaccin/corona/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class individue(models.Model):
+    nom=models.CharField(max_length=100,null=False)
+    prenom=models.CharField(max_length=100,null=False)
+    cin=models.CharField(max_length=100,null=False)
+    status=models.BooleanField(null=False,)
+class Question(models.Model):
+    question=models.CharField(max_length=1000,null=False)
--- vaccin/corona/views.py
+++ vaccin/corona/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def signin(request):
+    a=individue.objects.all()
+    temoin=0
+    for i in a:
+        if i.nom=="lamharchi" and i.prenom=="khalil":
+            temoin=1
+    print(temoin)
+
     return render(request,'signin.html')
