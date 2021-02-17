9bad226dc8929449adf5a7cd21632701f04332fe
khalillamharchi20 <khalillamharchi20@gmail.com>
2021-02-17 01:13:44 +0100

    4

--- vaccin/corona/views.py
+++ vaccin/corona/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from .models import *
 
 # Create your views here.
-def welcome(request):
-    return render(request,'signin.html')
-def prend_un_rendez_vous(request):
-    return render(request,'rendez_vous.html')
+def index(request):
+    return render(request,'index.html')
+def rdv(request):
+    return render(request,'rdv.html')
 
