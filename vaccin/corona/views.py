973e7fbe229435a59a9f9ca088741ba32864f041
abdessamad-abid <abdessamadabid7@gmail.com>
2021-02-26 22:24:37 +0100

    fix problem

--- vaccin/corona/views.py
+++ vaccin/corona/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template.loader import get_template
-from xhtml2pdf import pisa
 from django.core.mail import send_mail
 
 
